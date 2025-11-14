5078613355b2b8e4e68e61cde4c999f70fc1d499
Santiago Botero <157855016+LePeanutButter@users.noreply.github.com>
2025-11-13 19:50:00 -0500

    feat(infrastructure): implement payment provider integration for PayPal and Stripe

---
+++ API/Dtos.cs
@@ -0,0 +1,12 @@
+﻿namespace aquilosaurios_backend_core.API
+{
+    public class PaymentIntent(Guid purchaseId, double amount, string currency, string paymentToken)
+    {
+        public Guid PurchaseId { get; set; } = purchaseId;
+        public double Amount { get; set; } = amount;
+        public string Currency { get; set; } = currency;
+        public string? IdempotencyKey { get; set; }
+        public string PaymentToken { get; set; } = paymentToken;
+        public Dictionary<string, string>? Metadata { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using aquilosaurios_backend_core.Infrastructure.External;
+using DotNetEnv;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,6 +17,28 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+// Load environment variables from .env file
+Env.Load();
+
+// Conditionally register PayPal and Stripe providers based on the existence of configuration values
+if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PAYMENT_PROVIDERS_PAYPAL_CLIENTID")) &&
+    !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PAYMENT_PROVIDERS_PAYPAL_SECRET")))
+{
+    // Register PayPal provider only if both ClientId and Secret are provided
+    var paypalClientId = Environment.GetEnvironmentVariable("PAYMENT_PROVIDERS_PAYPAL_CLIENTID");
+    var paypalSecret = Environment.GetEnvironmentVariable("PAYMENT_PROVIDERS_PAYPAL_SECRET");
+    var paypalProvider = new PaypalProvider(paypalClientId, paypalSecret);
+    builder.Services.AddSingleton<IPaymentProvider>(paypalProvider);
+}
+
+if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PAYMENT_PROVIDERS_STRIPE_APIKEY")))
+{
+    // Register Stripe provider only if ApiKey is provided
+    var stripeApiKey = Environment.GetEnvironmentVariable("PAYMENT_PROVIDERS_STRIPE_APIKEY");
+    var stripeProvider = new StripeProvider(stripeApiKey);
+    builder.Services.AddSingleton<IPaymentProvider>(stripeProvider);
 }
 
 app.UseHttpsRedirection();
